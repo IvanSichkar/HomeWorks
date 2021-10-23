027b3e7c0980ada2ed3330ae1df5165f64af9e2c
IvanSichkar <sichkarig@belmilk.by>
2021-10-23 23:49:53 +0300

    Add Tasks 1-5

---
+++ HWorks/HW.06.Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace HW._06.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert poem:");
+            string poem = Console.ReadLine();
+
+            string[] arrPoem = poem.Split(";");
+
+            for(int i=0;i<arrPoem.Length;i++)
+            {
+                arrPoem[i]=arrPoem[i].Replace("o", "a",true,CultureInfo.CurrentCulture);
+            }
+
+            Console.WriteLine("Edit poem:");
+            foreach (var item in arrPoem)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ HWorks/HW.06.Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace HW._06.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countInvisibleSymb=0;
+
+            StreamReader textReader = new StreamReader(@"C:\Users\ivan\source\Repos\HomeWorks\HWorks\HW.06.Task4\FindMe.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            textReader.Dispose();
+
+            char[] arrayResult = textReaderResult.ToCharArray();
+
+            for(int i=0;i< arrayResult.Length;i++)
+            {
+                if (Char.IsControl(arrayResult[i]))
+                {
+                    Console.WriteLine(string.Format("U+{0:X4}", (int)arrayResult[i])+" position "+i);
+                    countInvisibleSymb++;
+                }                   
+            }
+            Console.WriteLine("Count invisible symbols: " + countInvisibleSymb);
+        }
+    }
+}
