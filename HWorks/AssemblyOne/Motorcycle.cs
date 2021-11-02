6c82a3abba592a277a6a72da7e52406da2cf98f6
IvanSichkar <sichkarig@belmilk.by>
2021-11-02 20:59:48 +0300

    Add Task1-3

---
+++ HWorks/AssemblyOne/Motorcycle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    public class Motorcycle  //public - для того, что бы класс был доступен в любом месте и в других сборках
+    {
+        int odometrDefault = 100;
+        public int odometrPublic = 100;
+        protected int odometrProtected = 100;
+        private int odometrPrivate = 100;
+        protected private int odometrProtectedPrivate = 100;
+        internal int odometrInternal = 100;
+        protected internal int odometrProtectedInternal = 100;
+
+        void StartEngineDefault() { }
+        public void StartEnginePublic() { }
+        protected void StartEngineProtected() { }
+        private void StartEnginePrivate() { }
+        protected private void PrivateStartNgineProtected() { }
+        internal void StartEngineInternal() { }
+        protected internal void StartEngineProtectedInternal() { }
+    }
+    public class SportBike : Motorcycle
+    {
+        public void SomeMtehod()
+        {
+            base.odometrInternal = 50;
+            base.odometrProtectedInternal = 50;
+            base.odometrProtectedPrivate = 50;
+            base.odometrPublic = 50;
+            base.odometrProtected = 50;
+
+            //base.odometrDefault=50; private - доступен только Motocycle
+            //base.odometrPrivate = 50; private - доступен только Motocycle
+        }
+    }
+}
---
+++ HWorks/AssemblyTwo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle moto = new Motorcycle();
+            moto.odometrPublic = 150;
+
+            //  moto.odometrPrivate = 10; // Доступен только Motorcycle
+            //  moto.odometrDefault = 10;// private - Доступен только Motorcycle
+            //  moto.odometrInternal = 10;// Доступен только в AssemblyOne
+            //  moto.odometrProtected = 10; // Доступен Motorcycle и его наследникам (и в других сборках) 
+            //  moto.odometrProtectedInternal=10; //Доступен в любом месте AssemblyOne и в наследниках Motorcycle 
+            //  moto.odometrProtectedPrivate=10; //Доступен Motorcycle и его наследникам в сборке AssemblyOnew
+
+            //у экземпляров sport и enduro - действуют такие же огрничения как и у экземпляра moto (см. выше)
+
+            SportBike sport = new SportBike();
+            sport.odometrPublic = 150;
+           
+            EnduroBike enduro = new EnduroBike();
+            enduro.odometrPublic = 150;
+        }
+        public class EnduroBike:Motorcycle
+        {
+            public void SomeMethod()
+            {
+                base.odometrProtected = 10; 
+                base.odometrProtectedInternal = 10;
+                base.odometrPublic = 10;
+
+                //base.odometrProtectedPrivate = 50;  Доступен Motorcycle и его наследникам в сборке AssemblyOne
+                //base.odometrInternal = 50; Доступен только в AssemblyOne
+                //base.odometrDefault=50; private - Доступен только Motorcycle
+                //base.odometrPrivate = 50; private - Доступен только Motorcycle
+            }
+        }
+    }
+}
